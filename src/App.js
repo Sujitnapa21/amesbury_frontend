a67e7e3dfe670f4db3f201dee7d4e504252909c0
Sujitnapa21 <sujitnapa29@gmail.com>
2024-10-22 23:18:57 +0700

    prepared component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+// import React from "react";
+// import { BrowserRouter, Switch, Route } from "react-router-dom";
+// import { ToastContainer } from 'react-toastify';
+// import 'react-toastify/dist/ReactToastify.css';
+// import Login from './components/pages/Login.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// function App() {
+//   return (
+//     <BrowserRouter>
+//       <div className="App">
+//         < ToastContainer position="top-center" />
+//         {/* <Switch></Switch> */}
+//           <Route exact path="/" component={Login} />
+        
+//       </div>
+//     </BrowserRouter>
+//   );
+// }
 
-export default App;
+// export default App;
---
+++ src/components/pages/Login.js
@@ -0,0 +1,8 @@
+import "../../App.js";
+export default function Login() {
+  return (
+    <div className="App">
+      <header className="App-header">AMESBURY COMPANY</header>
+    </div>
+  );
+}
